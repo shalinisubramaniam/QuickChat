77eb88b4f77a3bdf2d26277d8891836f68e2a10a
shalinisubramaniam <shalinisubramaniam234@gmail.com>
2025-07-07 10:52:53 +0530

    done routing for pages using routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
-
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+        <Route path='/login' element={<LoginPage />}/>
+        <Route path='/profile' element={<ProfilePage />}/>
+      </Routes>
     </div>
   )
 }
