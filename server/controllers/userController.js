cdcef9d902abfecf3afa869db9bfb79c2cd8cbfb
shalinisubramaniam <shalinisubramaniam234@gmail.com>
2025-07-11 16:08:52 +0530

    created "signup" controller function

---
+++ server/controllers/userController.js
@@ -0,0 +1,36 @@
+import { generateToken } from "../lib/utils";
+import User from "../models/User";
+import bcrypt from "bcryptjs";
+
+
+// Signup a new user
+export const signup = async (req, res)=>{
+    const { fullName, email, password, bio} = req.body;
+
+    try {
+        if(!fullName || !email || !password || !bio){
+            return res.json({success: false, message: "Missing Deatils"})
+        }
+        const user = await User.findOne({email});
+
+        if(user) {
+            return res.json({success: false, message: "Account already exists"})
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await User.create({
+            fullName, email, password: hashedPassword, bio
+        });
+
+        const token = generateToken(newUser._id)
+
+        res.json({success: true, userData: newUser, token, message: "Account created successfully"})
+        
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, userData: newUser, token, message: error.message})
+    }
+}
+
