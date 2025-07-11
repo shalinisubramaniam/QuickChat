3f2b641ced67ef440c47df110dc25e8f6331af41
shalinisubramaniam <shalinisubramaniam234@gmail.com>
2025-07-11 16:09:40 +0530

    created "login" controller function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -34,3 +34,23 @@ export const signup = async (req, res)=>{
     }
 }
 
+// Controller to login a user
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const userData = await User.findOne({email})
+
+        const isPasswordCorrect = await bcrypt.compare(password, userData.password);
+
+        if(!isPasswordCorrect) {
+            return res.json({success: false, message: "Invalid credentials"});
+        }
+
+        const token = generateToken(userData._id)
+
+        res.json({success: true, userData: userData, token, message: "Login successful"})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, userData: newUser, token, message: error.message})
+    }
+}
