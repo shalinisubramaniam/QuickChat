5b5c62e6ea18c45287d864ce52b7d2b96e64e58c
shalinisubramaniam <shalinisubramaniam234@gmail.com>
2025-07-21 22:09:04 +0530

    implemented getMessages function

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -23,3 +23,24 @@ export const getUserForSidebar = async (req, res)=>{
         res.json({success: false, message: error.messages})
     }
 }
+
+// Get all messages for selected user
+export const getMessages = async (req, res) => {
+    try {
+        const { id: selectedUserId } = req.params;
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                {senderId: myId, receiverId: selectedUserId},
+                {senderId: selectedUserId, receiverId: myId},
+            ]
+        })
+        await Message.updateMany({senderId: selectedUserId, receiverId: myId}, {seen: true});
+
+        res.json({success: true, messages})
+    } catch (error) {
+        console.log(error.messages);
+        res.json({success: false, message: error.messages})
+    }
+}
