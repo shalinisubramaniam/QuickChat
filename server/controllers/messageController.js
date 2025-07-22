1e6a89117d8ee0e504500b5f099109bbebfbe753
shalinisubramaniam <shalinisubramaniam234@gmail.com>
2025-07-22 22:37:04 +0530

    implemented sendMessage controller function

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,6 +1,6 @@
 import Message from "../models/message.js";
 import User from "../models/User.js";
-
+import cloudinary from "../lib/cloudinary.js";
 
 // Get all users except the logged in user
 export const getUserForSidebar = async (req, res)=>{
@@ -51,6 +51,34 @@ export const markMessageAsSeen = async (req, res) => {
         const { id } = req.params;
         await Message.findByIdAndUpdate(id, {seen: true})
         res.json({success: true})
+    } catch (error) {
+        console.log(error.messages);
+        res.json({success: false, message: error.messages})
+    }
+}
+
+// Send message to selected user
+export const sendMessage = async (req, res) => {
+    try {
+        const {text, image} = req.body;
+        const receiverId = req.params.id;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image){
+            const uploadResponse = await cloudinary.uploader.upload(image)
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = await Message.create({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        })
+
+        res.json({success: true, newMessage});
+
     } catch (error) {
         console.log(error.messages);
         res.json({success: false, message: error.messages})
