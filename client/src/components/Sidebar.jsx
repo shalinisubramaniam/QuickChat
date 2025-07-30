dab39b9ff43eda5a0b5c68fd96d399fbd60b48fc
shalinisubramaniam <shalinisubramaniam234@gmail.com>
2025-07-30 15:14:01 +0530

    logout implementation

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,7 +1,12 @@
 import { useNavigate } from "react-router-dom"
 import assets, { userDummyData } from "../assets/assets"
+import { useContext } from "react";
+import { AuthContext } from '../../context/AuthContext'
 
 const Sidebar = ({selectedUser, setSelectedUser}) => {
+
+    const { logout } = useContext(AuthContext)
+
   const navigate = useNavigate();
   
     return (
@@ -16,7 +21,7 @@ const Sidebar = ({selectedUser, setSelectedUser}) => {
                 bg-[#282142] border border-gray-600 text-gray-100 hidden group-hover:block">
                     <p onClick={()=>navigate('/profile')} className="cursor-pointer text-sm">Edit Profile</p>
                     <hr className="my-2 border-t border-gray-500" />
-                    <p className="cursor-pointer text-sm">Logout</p>
+                    <p onClick={()=>logout()} className="cursor-pointer text-sm">Logout</p>
                 </div>
             </div>
         </div>
