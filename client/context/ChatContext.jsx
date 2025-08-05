b57e91c571c21c56823666aa35b29a1900a7c5c2
shalinisubramaniam <shalinisubramaniam234@gmail.com>
2025-08-05 21:40:09 +0530

    implemented messages get & handler function

--- client/context/ChatContext.jsx
+++ client/context/ChatContext.jsx
@@ -85,7 +85,7 @@ export const ChatProvider = ({ children })=>{
     },[socket, selectedUser])
 
     const value = {
-        messages, users, selectedUser, getUsers, setMessages, sendMessage,
+        messages, users, selectedUser, getUsers, getMessages, sendMessage,
         setSelectedUser, unseenMessages, setUnseenMessages
     }
 
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -16,7 +16,7 @@ const HomePage = () => {
         'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' :
         'md:grid-cols-2' } `}>
         <Sidebar />
-        <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
+        <ChatContainer />
         <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
       </div>
     </div>
